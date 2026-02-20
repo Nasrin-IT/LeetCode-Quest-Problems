9c23671335dc3d7c6d9fb6672aabcdb0fc2a74af
NASRIN BEGAM Z <meowunicorn22@gmail.com>
2026-02-20 15:44:05 +0530

    Delete DataBase/SQL Basic Query directory

--- DataBase/SQL Basic Query/Prblm-1 : Combine 2 Tables .java
+++
@@ -1,9 +0,0 @@
-Select 
-
-p.firstname,
-p.lastname,
-a.city,
-a.state,
-From Person p //use Person table as a main source 
-LEFT JOIN Address a //combine address table while keeping all person records
-ON p.PersonId = a.PersonId //Matches rows Where personId is the same in both tables
